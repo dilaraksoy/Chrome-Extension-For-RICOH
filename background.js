b8836c360c0e1e78f39c837bc2039fd2f37a9d3a
dilaraksoy <dilaraaksoed@gmail.com>
2022-08-01 10:28:37 +0300

    Excel Problem in Server.js

--- background.js
+++ background.js
@@ -1,59 +1,47 @@
-
-chrome.tabs.query({
+chrome.tabs.query(
+  {
     active: true,
-    lastFocusedWindow: true
-}, function(tabs) {
+    lastFocusedWindow: true,
+  },
+  function (tabs) {
     var tab = tabs[0];
-});
+  }
+);
 
-let i=0;
-  var toggle = false;
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    
-    toggle = !toggle;
-    if(toggle){
-        alert("The extension is on!")
-        
-        chrome.tabs.query({
-            active: true,
-            lastFocusedWindow: true
-        }, function(tabs) {
-            // and use that tab to fill in out title and url
-            var tab = tabs[0];
-            var mySiteUrl='https://ricohspaces.app/en/home';
-              if(tab.url===mySiteUrl){
-                
-                
-                alert("Please click again to enter your surname!");
-                
-                
-                  chrome.browserAction.setPopup({popup:"./popup.html"})
-                  chrome.tabs.executeScript(null,{file:'./foreground.js'})  ;
-                  
-                  
-              
-              }
-              else{
-                 
-                  if(i===0){
-                      alert("This extension is just for RICOH!");
-                  };
-                  i=1;
-                  
-              }
-        });
-          var mySiteUrl='https://ricohspaces.app/en/home';
-          
-          
-    
-      
-      chrome.browserAction.setIcon({path: "./icon.png", tabId:tab.id});
-      
-    }
-    else{
-      chrome.browserAction.setIcon({path: "./disable.png", tabId:tab.id});
-      alert('extension is off');
-      
-    }
-  });
-  
+let i = 0;
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function (tab) {
+  toggle = !toggle;
+  if (toggle) {
+    alert("The extension is on!");
+
+    chrome.tabs.query(
+      {
+        active: true,
+        lastFocusedWindow: true,
+      },
+      function (tabs) {
+        // and use that tab to fill in out title and url
+        var tab = tabs[0];
+        var mySiteUrl = "https://ricohspaces.app/en/home";
+        if (tab.url === mySiteUrl) {
+          alert("Please click again to enter your surname!");
+
+          chrome.browserAction.setPopup({ popup: "./popup.html" });
+          chrome.tabs.executeScript(null, { file: "./foreground.js" });
+        } else {
+          if (i === 0) {
+            alert("This extension is just for RICOH!");
+          }
+          i = 1;
+        }
+      }
+    );
+    var mySiteUrl = "https://ricohspaces.app/en/home";
+
+    chrome.browserAction.setIcon({ path: "./icon.png", tabId: tab.id });
+  } else {
+    chrome.browserAction.setIcon({ path: "./disable.png", tabId: tab.id });
+    alert("extension is off");
+  }
+});
